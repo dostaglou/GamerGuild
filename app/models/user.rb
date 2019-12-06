bb11e58eace48ac0add850438e10cbfe0a0cf50d
Dostaglou <dostaglou@gmail.com>
2019-12-06 09:06:39 +0900

    basic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+  before_validation username_generator
+  validates :first_name, :last_name, :username, presense: true
+
+  def username_generator
+    if self.username.nil? && self.first_name || self.last_name
+      self.username = "#{self.first_name}_#{self.last_name}"
+    end
+  end
 end
