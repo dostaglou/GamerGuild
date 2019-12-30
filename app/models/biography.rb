44b3f41753197da078a75c33e60b6c4ee91cba51
Dostaglou <dostaglou@gmail.com>
2019-12-30 22:49:53 +0900

    basic relations plus validation

--- app/models/biography.rb
+++ app/models/biography.rb
@@ -1,3 +1,4 @@
 class Biography < ApplicationRecord
   belongs_to :character
+  has_many :chapters
 end
--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,3 +1,6 @@
 class Chapter < ApplicationRecord
   belongs_to :biography
+  has_many :biographies
+
+  validates :title, :content, :order, presence: true
 end
