20200f28e7e81908bdf2bd11b5e630542b38c481
Dostaglou <dostaglou@gmail.com>
2019-12-30 22:17:03 +0900

    msg

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
-  before_validation username_generator
-  validates :first_name, :last_name, :username, :email, :password, presense: true
 
-  def username_generator
-    if self.username.nil? && self.first_name || self.last_name
-      self.username = "#{self.first_name}_#{self.last_name}"
-    end
-  end
+  validates :first_name, :last_name, :username, :email, :password, presence: true
+
 end
--- db/migrate/20191205234157_devise_create_users.rb
+++ db/migrate/20191205234157_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
 
-      t.string  :first_name
-      t.string  :last_name
-      t.string  :username
-      t.text    :bio
-      t.string  :location
+      t.string     :first_name
+      t.string     :last_name
+      t.string     :username
+      t.text       :bio
+      t.string     :location
 
       ## Database authenticatable
       t.string :email,              null: false, default: ""
